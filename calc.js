e51a6745fcb8bdb69fb77ef2e3c100473764a509
canitry <56244521+canitry@users.noreply.github.com>
2023-07-04 01:41:42 -0700

    button pressing is now actually registered

--- calc.js
+++ calc.js
@@ -1,28 +1,5 @@
-let answer = 0;
-let currOp = null;
-const calculator = document.querySelector("#calculator");
-const disp = calculator.querySelector("#display");
-const btns = calculator.querySelectorAll(".digit, .op, .ans");
-for(const btn of btns){
-    const bClass = btn.getAttribute('class');
-    const bId = btn.getAttribute('id');
-    switch (bClass){
-        case ".digit":
-            btn.addEventListener("click", digPressed(bId));
-            break;
-        case ".op":
-            btn.addEventListern("click", opPressed(bId));
-            break
-        case ".ans":
-            btn.addEventListener("click", ansPressed(bId));
-            break;
-        default:
-            print("button not taken care of");
-    }
-}
-
 function digPressed(id){
-    
+    console.log("digPressed" + id)
 }
 
 function opPressed(id){
@@ -31,4 +8,28 @@ function opPressed(id){
 
 function ansPressed(id){
 
+}
+
+let answer = 0;
+let currOp = null;
+const calculator = document.getElementById("calculator");
+const disp = calculator.querySelector("#display");
+const btns = calculator.querySelectorAll("button");
+for(const btn of btns){
+    const bClass = btn.getAttribute('class');
+    const bId = btn.getAttribute('id');
+    switch (bClass){
+        case "digit":
+            btn.addEventListener("click", () => {digPressed(bId)});
+            //console.log(bId)
+            break;
+        case "op":
+            btn.addEventListener("click", () => {opPressed(bId)});
+            break
+        case "ans":
+            btn.addEventListener("click", () => {ansPressed(bId)});
+            break;
+        default:
+            console.log(bClass + bId + "button not taken care of");
+    }
 }
